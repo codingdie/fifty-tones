5c37bb873346c101871948c24fb4c8d38b3ba80e
codingdie <codingdie@gmail.com>
2019-09-20 14:44:34 +0800

    update

--- generate.py
+++ generate.py
@@ -3,37 +3,38 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 # 随机字母:
-chars=u"あいうえおかきくけきさしすせそたちつてとなにぬねのはひふへほまみむめもやいゆえよ";
+pingjia=u"あいうえおかきくけきさしすせそたちつてとなにぬねのはひふへほまみむめもやいゆえよらりるれろわいうえを";
+pianjia=u"アイウエオカキクケコサィシセソタチツテトナニヌネノハヒフヘホマミムメモヤイユエヨラリルレロワイウエヲ";
 last=''
-def rndChar():
-    char=chars[random.randint(0, len(chars)-1)]
+def rndChar(chars):
+    char=chars[random.randint(0, len(pingjia)-1)]
     if(char!=last):
         return char
-    
     return rndChar()
 
 # 随机颜色1:
 def rndColor():
     return (random.randint(64, 255), random.randint(64, 255), random.randint(64, 255))
 
-# 随机颜色2:
-def rndColor2():
-    return (0, 0, 0)
 
 width = 15
-height = 40
+height = 20
 wl=120
-hl=60
-image = Image.new('RGB', ((width+2)*wl, (height+2)*hl), (255, 255, 255))
-# 创建Font对象:
-font = ImageFont.truetype("游明朝体.ttf" ,40)
-font1 = ImageFont.truetype("Arial Unicode.ttf", 36)
-# 创建Draw对象:
-draw = ImageDraw.Draw(image)
+hl=130
 
-for i in range(width):
-    for j in range(height):
-        draw.text((wl * (i+1) , (j+1)*hl), rndChar(), font=font, fill=rndColor2())
-# 模糊:
-# image = image.filter(ImageFilter.BLUR)
-image.save('code.jpg', 'jpeg');
+def generate(file,chars):
+    image = Image.new('RGB', ((width+2)*wl, (height+1)*hl), (255, 255, 255))
+    # 创建Font对象:
+    font = ImageFont.truetype("游明朝体.ttf" ,50)
+    # font1 = ImageFont.truetype("Arial Unicode.ttf", 36)
+    # 创建Draw对象:
+    draw = ImageDraw.Draw(image)
+    for i in range(width):
+        for j in range(height):
+            draw.text((wl * (i+1) , (j+1)*hl), rndChar(chars), font=font, fill= (0, 0, 0))
+    # 模糊:
+    # image = image.filter(ImageFilter.BLUR)
+    image.save(file, 'jpeg');
+
+generate('pingjia.jpg',pianjia);
+generate('pianjia.jpg',pianjia);
